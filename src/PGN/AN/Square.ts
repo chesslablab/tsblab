0c66fcf346331d611c0497b47bf82ae9569b031a
programarivm <info@programarivm.com>
2022-04-21 23:37:36 +0200

    A new test is written

--- src/PGN/AN/Square.ts
+++ src/PGN/AN/Square.ts
@@ -4,11 +4,11 @@ class Square {
   static readonly regex = '[a-h]{1}[1-8]{1}';
 
   public static validate = (value: string): string => {
-    if (new RegExp(Square.regex).test(value)) {
-      return value;
+    if (!new RegExp(Square.regex).test(value)) {
+      throw new UnknownNotationError;
     }
 
-    throw new UnknownNotationError;
+    return value;
   };
 }
 
--- tests/unit/PGN/AN/Square.test.ts
+++ tests/unit/PGN/AN/Square.test.ts
@@ -3,6 +3,9 @@ import Square from '../../../../src/PGN/AN/Square';
 import { expect } from 'chai';
 
 describe('Square.validate()', () => {
+  it ('a9 throws UnknownNotationError', () => {
+    expect(() => Square.validate('a9')).to.throw(UnknownNotationError);
+  });
   it ('foo throws UnknownNotationError', () => {
     expect(() => Square.validate('foo')).to.throw(UnknownNotationError);
   });

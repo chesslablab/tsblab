0c28c3636a1485b2c113a802fe4b6b0fe8ab4453
Owais Hasnath Ahmed <37238759+owais34@users.noreply.github.com>
2022-04-28 20:05:25 +0530

    Added Piece.ts according to conventions

---
+++ src/PGN/AN/Piece.ts
@@ -0,0 +1,25 @@
+import AbstractNotation from "../AbstractNotation";
+
+
+class Piece extends AbstractNotation{
+    public static readonly B: string = 'B';
+    public static readonly K: string = 'K';
+    public static readonly N: string = 'N';
+    public static readonly P: string = 'P';
+    public static readonly Q: string = 'Q';
+    public static readonly R: string = 'R';
+
+    values=(): string[] => {
+        return [
+            Piece.B,
+            Piece.K,
+            Piece.N,
+            Piece.P,
+            Piece.Q,
+            Piece.R
+        ]
+    }
+    
+}
+
+export default Piece

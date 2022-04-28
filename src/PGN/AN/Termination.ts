45190dc59ff3b16160bb9822656fbc1254d48b12
Owais Hasnath Ahmed <37238759+owais34@users.noreply.github.com>
2022-04-28 20:25:56 +0530

    Added Termination.ts file with required specs

---
+++ src/PGN/AN/Termination.ts
@@ -0,0 +1,22 @@
+import AbstractNotation from "../AbstractNotation";
+
+
+class Termination extends AbstractNotation{
+
+    public static readonly WHITE_WINS: string = '1-0';
+    public static readonly BLACK_WINS: string = '0-1';
+    public static readonly DRAW: string = '1/2-1/2';
+    public static readonly UNKNOWN: string = '*';
+
+    values=(): string[] => {
+        return [
+            Termination.WHITE_WINS,
+            Termination.BLACK_WINS,
+            Termination.DRAW,
+            Termination.UNKNOWN
+        ]
+    }
+    
+}
+
+export default Termination

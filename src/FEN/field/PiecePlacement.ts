7e2777d885b61d21ee0a0261ea8eec02b94758cf
Owais Hasnath Ahmed <37238759+owais34@users.noreply.github.com>
2022-04-30 21:44:30 +0530

    Added PiecePlacement class according to the specifications

---
+++ src/FEN/field/PiecePlacement.ts
@@ -0,0 +1,20 @@
+import UnknownNotationError from "../../error/UnknownNotationError";
+import ValidationInterface from "../ValidationInterface";
+
+
+class PiecePlacement implements ValidationInterface{
+    
+    validate = (value: string): string => {
+        const fields=value.split("/");
+        const count=fields.length;
+        if(count===8){
+            return value;
+        }
+
+        throw new UnknownNotationError;
+    }
+    
+}
+
+export default PiecePlacement;
+

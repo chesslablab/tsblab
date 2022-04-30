cc9d4c19c4826e52d9a604a97673b5c69dd13a6f
Owais Hasnath Ahmed <37238759+owais34@users.noreply.github.com>
2022-04-30 23:20:54 +0530

    Fixed spacing in PiecePlacement.ts

--- src/FEN/field/PiecePlacement.ts
+++ src/FEN/field/PiecePlacement.ts
@@ -3,16 +3,15 @@ import ValidationInterface from "../ValidationInterface";
 
 
 class PiecePlacement implements ValidationInterface{
-    
-    validate = (value: string): string => {
-        const fields=value.split("/");
-        const count=fields.length;
-        if(count===8){
-            return value;
-        }
-
-        throw new UnknownNotationError;
+  validate = (value: string): string => {
+    const fields=value.split("/");
+    const count=fields.length;
+    if(count===8){
+      return value;
     }
+
+    throw new UnknownNotationError;
+  }
     
 }
 
--- tests/unit/FEN/field/PiecePlacement.test.ts
+++ tests/unit/FEN/field/PiecePlacement.test.ts
@@ -3,12 +3,12 @@ import UnknownNotationError from '../../../../src/error/UnknownNotationError';
 import PiecePlacement from '../../../../src/FEN/field/PiecePlacement';
 
 describe('PiecePlacement.validate()', () => {
-    const piecePlacement=new PiecePlacement();
-    it ('- throws UnknownNotationError', () => {
-        expect(() => piecePlacement.validate('-')).to.throw(UnknownNotationError);
-    });
-    it ('/////// is valid', () =>{
-        expect(piecePlacement.validate('///////')).to.equal('///////');
-    });
+  const piecePlacement=new PiecePlacement();
+  it ('- throws UnknownNotationError', () => {
+    expect(() => piecePlacement.validate('-')).to.throw(UnknownNotationError);
+  });
+  it ('/////// is valid', () =>{
+    expect(piecePlacement.validate('///////')).to.equal('///////');
+  });
 
-})
+});

3ba7f7ec9a61438c5e1458eb8e027c3868c181c8
programarivm <info@programarivm.com>
2022-04-30 13:18:32 +0200

    Sticked to a style guide

--- src/FEN/field/CastlingAbility.ts
+++ src/FEN/field/CastlingAbility.ts
@@ -1,74 +1,71 @@
-import UnknownNotationError from "../../error/UnknownNotationError";
-import Color from "../../PGN/AN/Color";
-import Piece from "../../PGN/AN/Piece";
-import ValidationInterface from "../ValidationInterface";
+import UnknownNotationError from '../../error/UnknownNotationError';
+import Color from '../../PGN/AN/Color';
+import Piece from '../../PGN/AN/Piece';
+import ValidationInterface from '../ValidationInterface';
 
 
-class CastlingAbility implements ValidationInterface{
+class CastlingAbility implements ValidationInterface {
+  public static readonly START: string = 'KQkq';
 
-    public static readonly START: string = 'KQkq';
+  public static readonly NEITHER: string = '-';
 
-    public static readonly NEITHER: string = '-';
-
-    validate = (value: string): string =>{
-        if(CastlingAbility.NEITHER===value){
-            return value
-        }
-        else if(value && value.match(/^K?Q?k?q?$/)){
-            return value
-        }
-        throw new UnknownNotationError;
+  validate = (value: string): string => {
+    if (CastlingAbility.NEITHER === value) {
+      return value;
+    } else if (value && value.match(/^K?Q?k?q?$/)) {
+      return value;
     }
 
-    remove = (castlingAbility: string,color: string,ids: string[]): string=>{
-        
-        if(color===Color.B){
-            ids=ids.map(id=>id.toLowerCase())
-        }
+    throw new UnknownNotationError;
+  }
 
-        castlingAbility=castlingAbility.replace(new RegExp(ids.join("|"),"g"),"")
-
-        if(!castlingAbility){
-            castlingAbility=CastlingAbility.NEITHER
-        }
-
-        return castlingAbility
+  remove = (castlingAbility: string, color: string, ids: string[]): string => {
+    if (color === Color.B) {
+      ids = ids.map(id => id.toLowerCase());
     }
-    
-    castle = (castlingAbility: string,color: string): string=>{
-
-        castlingAbility=this.remove(
-            castlingAbility,
-            color,
-            [Piece.K, Piece.Q])
-        return castlingAbility
+    castlingAbility = castlingAbility.replace(new RegExp(ids.join('|'),'g'),'');
+    if (!castlingAbility) {
+      castlingAbility=CastlingAbility.NEITHER;
     }
 
-    long = (castlingAbility: string,color: string):string=>{
+    return castlingAbility;
+  }
 
-        let id=Piece.Q
-        if(color===Color.B){
-            id=id.toLowerCase()
-        }
+  castle = (castlingAbility: string,color: string): string => {
+    castlingAbility = this.remove(
+      castlingAbility,
+      color,
+      [Piece.K, Piece.Q]
+    );
 
-        return castlingAbility.substring(castlingAbility.search(id)===-1?0:castlingAbility.search(id))
+    return castlingAbility;
+  }
+
+  long = (castlingAbility: string, color: string): string => {
+    let id = Piece.Q;
+    if (color === Color.B) {
+      id = id.toLowerCase();
     }
 
-    short = (castlingAbility: string,color: string):string=>{
+    return castlingAbility.substring(
+      castlingAbility.search(id) === -1 ? 0 : castlingAbility.search(id)
+    );
+  }
 
-        let id=Piece.K
-        if(color===Color.B){
-            id=id.toLowerCase()
-        }
-
-        return castlingAbility.substring(castlingAbility.search(id)===-1?0:castlingAbility.search(id))
+  short = (castlingAbility: string, color: string): string => {
+    let id = Piece.K;
+    if (color === Color.B) {
+      id = id.toLowerCase();
     }
 
-    can = (castlingAbility: string,color: string):string=>{
+    return castlingAbility.substring(
+      castlingAbility.search(id) === -1 ? 0 : castlingAbility.search(id)
+    );
+  }
 
-        return this.long(castlingAbility,color)||this.short(castlingAbility,color)
-    }
-
+  can = (castlingAbility: string, color: string): string => {
+    return this.long(castlingAbility,color) || this.short(castlingAbility,color);
+  }
 }
 
 export default CastlingAbility;
--- tests/unit/FEN/field/CastlingAbility.test.ts
+++ tests/unit/FEN/field/CastlingAbility.test.ts
@@ -3,112 +3,87 @@ import UnknownNotationError from '../../../../src/error/UnknownNotationError';
 import CastlingAbility from '../../../../src/FEN/field/CastlingAbility';
 import Piece from '../../../../src/PGN/AN/Piece';
 
-describe('CastlingAbility.validate()',()=>{
-    const castlingAbility=new CastlingAbility();
-
-    it('foobar throws UnknownNotationError',()=>{
-        expect(()=>castlingAbility.validate('foobar')).to.throw(UnknownNotationError);
-    });
-
-    it('kqKQ throws UnknownNotationError',()=>{
-        expect(()=>castlingAbility.validate('kqKQ')).to.throw(UnknownNotationError);
-    });
-
-    it('KkQq throws UnknownNotationError',()=>{
-        expect(()=>castlingAbility.validate('KkQq')).to.throw(UnknownNotationError);
-    });
-    
-    it('-- throws UnknownNotationError',()=>{
-        expect(()=>castlingAbility.validate('--')).to.throw(UnknownNotationError);
-    });
-
-    it('k- throws UnknownNotationError',()=>{
-        expect(()=>castlingAbility.validate('k-')).to.throw(UnknownNotationError);
-    });
-
-    it('empty string throws UnknownNotationError',()=>{
-        expect(()=>castlingAbility.validate('')).to.throw(UnknownNotationError);
-    });
-
-    it('KQkq is valid',()=>{
-        expect(castlingAbility.validate('KQkq')).to.equal('KQkq');
-    });
-
-    it('K is valid',()=>{
-        expect(castlingAbility.validate('K')).to.equal('K');
-    });
-
-    it('Q is valid',()=>{
-        expect(castlingAbility.validate('Q')).to.equal('Q');
-    });
-
-    it('k is valid',()=>{
-        expect(castlingAbility.validate('k')).to.equal('k');
-    });
-
-    it('q is valid',()=>{
-        expect(castlingAbility.validate('q')).to.equal('q');
-    });
-
-    it('KQ is valid',()=>{
-        expect(castlingAbility.validate('KQ')).to.equal('KQ');
-    });
-
-    it('- is valid',()=>{
-        expect(castlingAbility.validate('-')).to.equal('-');
-    });
+describe('CastlingAbility.validate()', () => {
+  const castlingAbility = new CastlingAbility();
+  it ('foobar throws UnknownNotationError', () => {
+    expect(() => castlingAbility.validate('foobar')).to.throw(UnknownNotationError);
+  });
+  it ('kqKQ throws UnknownNotationError', () => {
+    expect(() => castlingAbility.validate('kqKQ')).to.throw(UnknownNotationError);
+  });
+  it ('KkQq throws UnknownNotationError', () => {
+    expect(() => castlingAbility.validate('KkQq')).to.throw(UnknownNotationError);
+  });
+  it ('-- throws UnknownNotationError', () => {
+    expect(() => castlingAbility.validate('--')).to.throw(UnknownNotationError);
+  });
+  it ('k- throws UnknownNotationError', () => {
+    expect(() => castlingAbility.validate('k-')).to.throw(UnknownNotationError);
+  });
+  it ('empty string throws UnknownNotationError', () => {
+    expect(() => castlingAbility.validate('')).to.throw(UnknownNotationError);
+  });
+  it ('KQkq is valid', () => {
+    expect(castlingAbility.validate('KQkq')).to.equal('KQkq');
+  });
+  it ('K is valid', () => {
+    expect(castlingAbility.validate('K')).to.equal('K');
+  });
+  it ('Q is valid', () => {
+    expect(castlingAbility.validate('Q')).to.equal('Q');
+  });
+  it ('k is valid', () => {
+    expect(castlingAbility.validate('k')).to.equal('k');
+  });
+  it ('q is valid', () => {
+    expect(castlingAbility.validate('q')).to.equal('q');
+  });
+  it ('KQ is valid', () => {
+    expect(castlingAbility.validate('KQ')).to.equal('KQ');
+  });
+  it ('- is valid', () => {
+    expect(castlingAbility.validate('-')).to.equal('-');
+  });
 })
 
-describe('CastlingAbility.remove()',()=>{
-    const castlingAbility=new CastlingAbility();
-
-    it('Remove w_K from KQkq',()=>{
-        
-        expect(castlingAbility.remove('KQkq','w',[Piece.K])).to.equal('Qkq');
-    });
-
-    it('Remove w_Q from KQkq',()=>{
-        expect(castlingAbility.remove('KQkq','w',[Piece.Q])).to.equal('Kkq');
-    });
-
-    it('Remove b_k from KQkq',()=>{
-        expect(castlingAbility.remove('KQkq','b',[Piece.K])).to.equal('KQq');
-    });
-
-    it('Remove b_q from KQkq',()=>{
-        expect(castlingAbility.remove('KQkq','b',[Piece.Q])).to.equal('KQk');
-    });
-
-    it('Remove w_K_Q from KQkq',()=>{
-        expect(castlingAbility.remove('KQkq','w',[Piece.Q,Piece.K])).to.equal('kq');
-    });
-
-    it('Remove b_k_q from KQkq',()=>{
-        expect(castlingAbility.remove('KQkq','b',[Piece.Q,Piece.K])).to.equal('KQ');
-    });
+describe('CastlingAbility.remove()', () => {
+  const castlingAbility = new CastlingAbility();
+  it ('Remove w_K from KQkq', () => {
+    expect(castlingAbility.remove('KQkq','w',[Piece.K])).to.equal('Qkq');
+  });
+  it ('Remove w_Q from KQkq', () => {
+    expect(castlingAbility.remove('KQkq','w',[Piece.Q])).to.equal('Kkq');
+  });
+  it ('Remove b_k from KQkq', () => {
+    expect(castlingAbility.remove('KQkq','b',[Piece.K])).to.equal('KQq');
+  });
+  it ('Remove b_q from KQkq', () => {
+    expect(castlingAbility.remove('KQkq','b',[Piece.Q])).to.equal('KQk');
+  });
+  it ('Remove w_K_Q from KQkq', () => {
+    expect(castlingAbility.remove('KQkq','w',[Piece.Q,Piece.K])).to.equal('kq');
+  });
+  it ('Remove b_k_q from KQkq', () => {
+    expect(castlingAbility.remove('KQkq','b',[Piece.Q,Piece.K])).to.equal('KQ');
+  });
 })
 
-describe('CastlingAbility.castle()',()=>{
-
-    const castlingAbility=new CastlingAbility();
-
-    it('Castle w from KQkq',()=>{
-        expect(castlingAbility.castle('KQkq','w')).to.equal('kq');
-    });
-
-    it('Castle b from KQkq',()=>{
-        expect(castlingAbility.castle('KQkq','b')).to.equal('KQ');
-    });
-
-    it('Castle w_b from KQkq',()=>{
-        let castled=castlingAbility.castle('KQkq','w')
-        castled=castlingAbility.castle(castled,'b')
-        expect(castled).to.equal('-');
-    });
-
-    it('Castle b_w from KQkq',()=>{
-        let castled=castlingAbility.castle('KQkq','b')
-        castled=castlingAbility.castle(castled,'w')
-        expect(castled).to.equal('-');
-    });
+describe('CastlingAbility.castle()', () => {
+  const castlingAbility = new CastlingAbility();
+  it ('Castle w from KQkq', () => {
+    expect(castlingAbility.castle('KQkq','w')).to.equal('kq');
+  });
+  it ('Castle b from KQkq', () => {
+    expect(castlingAbility.castle('KQkq','b')).to.equal('KQ');
+  });
+  it ('Castle w_b from KQkq', () => {
+    let castled = castlingAbility.castle('KQkq','w')
+    castled = castlingAbility.castle(castled,'b')
+    expect(castled).to.equal('-');
+  });
+  it ('Castle b_w from KQkq', () => {
+    let castled = castlingAbility.castle('KQkq','b')
+    castled = castlingAbility.castle(castled,'w')
+    expect(castled).to.equal('-');
+  });
 })

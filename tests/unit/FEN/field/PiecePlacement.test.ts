b26ab10b0ab05f6b21b6691fdacbfe6b22d7baac
Owais Hasnath Ahmed <37238759+owais34@users.noreply.github.com>
2022-04-30 22:58:08 +0530

    Added basic tests for PiecePlacement class

---
+++ tests/unit/FEN/field/PiecePlacement.test.ts
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+import UnknownNotationError from '../../../../src/error/UnknownNotationError';
+import PiecePlacement from '../../../../src/FEN/field/PiecePlacement';
+
+describe('PiecePlacement.validate()', () => {
+    const piecePlacement=new PiecePlacement();
+    it ('- throws UnknownNotationError', () => {
+        expect(() => piecePlacement.validate('-')).to.throw(UnknownNotationError);
+    });
+    it ('/////// is valid', () =>{
+        expect(piecePlacement.validate('///////')).to.equal('///////')
+    })
+
+})

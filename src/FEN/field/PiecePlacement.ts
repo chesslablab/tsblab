d87c1b5f37791d386d04713ca23abe5d3dc02b72
Owais Hasnath Ahmed <37238759+owais34@users.noreply.github.com>
2022-05-02 21:18:38 +0530

    Fixed spacing in variable assignments

--- src/FEN/field/PiecePlacement.ts
+++ src/FEN/field/PiecePlacement.ts
@@ -1,12 +1,11 @@
 import UnknownNotationError from "../../error/UnknownNotationError";
 import ValidationInterface from "../ValidationInterface";
 
-
 class PiecePlacement implements ValidationInterface{
   validate = (value: string): string => {
-    const fields=value.split("/");
-    const count=fields.length;
-    if(count===8){
+    const fields = value.split("/");
+    const count = fields.length;
+    if (count === 8) {
       return value;
     }
 

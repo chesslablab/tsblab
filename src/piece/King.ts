c136f2c65d31519e88635dc5177b302fa0df3347
programarivm <info@programarivm.com>
2022-05-02 15:57:00 +0200

    TDDed the King

--- src/piece/King.ts
+++ src/piece/King.ts
@@ -20,6 +20,52 @@ class King {
             'next': 'c1'
           }
         }
+      },
+      [Piece.R]: {
+        [Castle.SHORT]: {
+          'sq': {
+            'current': 'h1',
+            'next': 'f1'
+          }
+        },
+        [Castle.LONG]: {
+          'sq': {
+            'current': 'a1',
+            'next': 'd1'
+          }
+        }
+      },
+    },
+    [Color.B]: {
+      [Piece.K]: {
+        [Castle.SHORT]: {
+          'sqs': [ 'f8', 'g8' ],
+          'sq': {
+            'current': 'e8',
+            'next': 'g8'
+          }
+        },
+        [Castle.LONG]: {
+          'sqs': [ 'b8', 'c8', 'd8' ],
+          'sq': {
+            'current': 'e8',
+            'next': 'c8'
+          }
+        }
+      },
+      [Piece.R]: {
+        [Castle.SHORT]: {
+          'sq': {
+            'current': 'h8',
+            'next': 'f8'
+          }
+        },
+        [Castle.LONG]: {
+          'sq': {
+            'current': 'a8',
+            'next': 'd8'
+          }
+        }
       }
     }
   }
--- tests/unit/piece/King.test.ts
+++ tests/unit/piece/King.test.ts
@@ -5,11 +5,20 @@ import Piece from '../../../src/PGN/AN/Piece';
 import King from '../../../src/piece/King';
 
 describe('King.CASTLING_RULE', () => {
-  const rule = King.CASTLING_RULE[Color.W];
   it ('w Castle.LONG sqs', () => {
-    expect(rule[Piece.K][Castle.LONG].sqs).to.eql([ 'b1', 'c1', 'd1' ]);
+    expect(King.CASTLING_RULE[Color.W][Piece.K][Castle.LONG].sqs)
+      .to.eql([ 'b1', 'c1', 'd1' ]);
+  });
+  it ('b Castle.LONG sqs', () => {
+    expect(King.CASTLING_RULE[Color.B][Piece.K][Castle.LONG].sqs)
+      .to.eql([ 'b8', 'c8', 'd8' ]);
   });
   it ('w Castle.SHORT sqs', () => {
-    expect(rule[Piece.K][Castle.SHORT].sqs).to.eql([ 'f1', 'g1' ]);
+    expect(King.CASTLING_RULE[Color.W][Piece.K][Castle.SHORT].sqs)
+      .to.eql([ 'f1', 'g1' ]);
+  });
+  it ('b Castle.SHORT sqs', () => {
+    expect(King.CASTLING_RULE[Color.B][Piece.K][Castle.SHORT].sqs)
+      .to.eql([ 'f8', 'g8' ]);
   });
 });

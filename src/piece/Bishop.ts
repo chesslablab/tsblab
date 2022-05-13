b1b4abb20864f2556fa6b612c16549b4ff315209
Owais Hasnath Ahmed <37238759+owais34@users.noreply.github.com>
2022-05-14 00:54:03 +0530

    added bishop class and unit test in accordance with spec

---
+++ src/piece/Bishop.ts
@@ -0,0 +1,68 @@
+import AbstractPiece from "./AbstractPiece";
+import Square from '../PGN/AN/Square'
+
+class Bishop extends AbstractPiece {
+  constructor(color: string, sq: string) {
+    super(color, sq);
+    this.mobility = {
+      upLeft: [],
+      upRight: [],
+      bottomLeft: [],
+      bottonRight: []
+    };
+
+    this.calcMobility()
+  }
+
+  protected calcMobility(): AbstractPiece {
+    try {
+      let file = String.fromCharCode(this.sq[0].charCodeAt(0) - 1);
+      let rank = String.fromCharCode(this.sq[1].charCodeAt(0) + 1);
+      while (new Square().validate(file + rank)) {
+        this.mobility["upLeft"].push(file + rank);
+        file = String.fromCharCode(file.charCodeAt(0) - 1);
+        rank = String.fromCharCode(rank.charCodeAt(0) + 1);
+      }
+    } catch (e) {
+      
+    }
+    try {
+      let file = String.fromCharCode(this.sq[0].charCodeAt(0) + 1);
+      let rank = String.fromCharCode(this.sq[1].charCodeAt(0) + 1);
+      while (new Square().validate(file + rank)) {
+        this.mobility["upRight"].push(file + rank);
+        file = String.fromCharCode(file.charCodeAt(0) + 1);
+        rank = String.fromCharCode(rank.charCodeAt(0) + 1);
+      }
+    } catch (e) {
+      
+    }
+    try {
+      let file = String.fromCharCode(this.sq[0].charCodeAt(0) - 1);
+      let rank = String.fromCharCode(this.sq[1].charCodeAt(0) - 1);
+      while (new Square().validate(file + rank)) {
+        this.mobility["bottonLeft"].push(file + rank);
+        file = String.fromCharCode(file.charCodeAt(0) - 1);
+        rank = String.fromCharCode(rank.charCodeAt(0) - 1);
+      }
+    } catch (e) {
+      
+    }
+    try {
+      let file = String.fromCharCode(this.sq[0].charCodeAt(0) + 1);
+      let rank = String.fromCharCode(this.sq[1].charCodeAt(0) - 1);
+      while (new Square().validate(file + rank)) {
+        this.mobility["bottomRight"].push(file + rank);
+        file = String.fromCharCode(file.charCodeAt(0) + 1);
+        rank = String.fromCharCode(rank.charCodeAt(0) - 1);
+      }
+    } catch (e) {
+      
+    }
+
+    return this;
+  }
+  
+}
+
+export default Bishop;

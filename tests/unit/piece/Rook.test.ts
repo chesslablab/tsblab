c70619e5264cf42cc3a8f4efafa0be524e6d28a0
Owais Hasnath Ahmed <37238759+owais34@users.noreply.github.com>
2022-05-16 23:47:08 +0530

    reconfigured Rook test

--- tests/unit/piece/Rook.test.ts
+++ tests/unit/piece/Rook.test.ts
@@ -1,34 +1,25 @@
 import { expect } from 'chai';
 import Rook from '../../../src/piece/Rook';
+import RookType from '../../../src/piece/RookType';
 
 describe('Rook.getMobility()', () => {
   it ('a2', () => {
-    let rook = new Rook('w', 'a2');
+    let rook = new Rook('w', 'a2', RookType.PROMOTED);
     let mobility = {
-      upLeft: [],
-      upRight: ['b3', 'c4', 'd5', 'e6', 'f7', 'g8'],
-      bottomLeft: [],
-      bottomRight: ['b1']
+      up: ['a3', 'a4', 'a5', 'a6', 'a7', 'a8'],
+      bottom: ['a1'],
+      left: [],
+      right: ['b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2']
     };
     expect(rook.getMobility()).to.eql(mobility);
   });
   it ('d5', () => {
-    let rook = new Rook('w', 'd5');
+    let rook = new Rook('w', 'd5', RookType.PROMOTED);
     let mobility = {
-      upLeft: ['c6', 'b7', 'a8'],
-      upRight: ['e6', 'f7', 'g8'],
-      bottomLeft: ['c4', 'b3', 'a2'],
-      bottomRight: ['e4', 'f3', 'g2', 'h1']
-    };
-    expect(rook.getMobility()).to.eql(mobility);
-  });
-  it ('a8', () => {
-    let rook = new Rook('w', 'a8');
-    let mobility = {
-      upLeft: [],
-      upRight: [],
-      bottomLeft: [],
-      bottomRight: ['b7', 'c6', 'd5', 'e4', 'f3', 'g2', 'h1']
+      up: ['d6', 'd7', 'd8'],
+      bottom: ['d4', 'd3', 'd2', 'd1'],
+      left: ['c5', 'b5', 'a5'],
+      right: ['e5', 'f5', 'g5', 'h5']
     };
     expect(rook.getMobility()).to.eql(mobility);
   });

66719c626821fd909fbd7349c130ac834773c6af
programarivm <info@programarivm.com>
2022-06-17 12:15:45 +0200

    Refactored Board

--- src/Board.ts
+++ src/Board.ts
@@ -20,14 +20,14 @@ class Board extends Map {
     this.set(5, new Bishop(Color.W, 'f1'));
     this.set(6, new Knight(Color.W, 'g1'));
     this.set(7, new Rook(Color.W, 'h1', RookType.CASTLE_SHORT));
-    this.set(8, new Knight(Color.W, 'a2'));
-    this.set(9, new Knight(Color.W, 'b2'));
-    this.set(10, new Knight(Color.W, 'c2'));
-    this.set(11, new Knight(Color.W, 'd2'));
-    this.set(12, new Knight(Color.W, 'e2'));
-    this.set(13, new Knight(Color.W, 'f2'));
-    this.set(14, new Knight(Color.W, 'g2'));
-    this.set(15, new Knight(Color.W, 'h2'));
+    this.set(8, new Pawn(Color.W, 'a2'));
+    this.set(9, new Pawn(Color.W, 'b2'));
+    this.set(10, new Pawn(Color.W, 'c2'));
+    this.set(11, new Pawn(Color.W, 'd2'));
+    this.set(12, new Pawn(Color.W, 'e2'));
+    this.set(13, new Pawn(Color.W, 'f2'));
+    this.set(14, new Pawn(Color.W, 'g2'));
+    this.set(15, new Pawn(Color.W, 'h2'));
     this.set(16, new Rook(Color.B, 'a8', RookType.CASTLE_LONG));
     this.set(17, new Knight(Color.B, 'b8'));
     this.set(18, new Bishop(Color.B, 'c8'));
@@ -36,16 +36,15 @@ class Board extends Map {
     this.set(21, new Bishop(Color.B, 'f8'));
     this.set(22, new Knight(Color.B, 'g8'));
     this.set(23, new Rook(Color.B, 'h8', RookType.CASTLE_SHORT));
-    this.set(24, new Knight(Color.B, 'a7'));
-    this.set(25, new Knight(Color.B, 'b7'));
-    this.set(26, new Knight(Color.B, 'c7'));
-    this.set(27, new Knight(Color.B, 'd7'));
-    this.set(28, new Knight(Color.B, 'e7'));
-    this.set(29, new Knight(Color.B, 'f7'));
-    this.set(30, new Knight(Color.B, 'g7'));
-    this.set(31, new Knight(Color.B, 'h7'));
+    this.set(24, new Pawn(Color.B, 'a7'));
+    this.set(25, new Pawn(Color.B, 'b7'));
+    this.set(26, new Pawn(Color.B, 'c7'));
+    this.set(27, new Pawn(Color.B, 'd7'));
+    this.set(28, new Pawn(Color.B, 'e7'));
+    this.set(29, new Pawn(Color.B, 'f7'));
+    this.set(30, new Pawn(Color.B, 'g7'));
+    this.set(31, new Pawn(Color.B, 'h7'));
   }
-
 }
 
 export default Board;

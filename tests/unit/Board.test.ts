8f01873cb49cd0b4760c36bffd571fc578f491c5
programarivm <info@programarivm.com>
2022-06-22 16:58:32 +0200

    Rewrote Board.test.ts

--- tests/unit/Board.test.ts
+++ tests/unit/Board.test.ts
@@ -3,21 +3,19 @@ import Piece from '../../src/PGN/AN/Piece';
 import Board from '../../src/Board';
 
 describe('new Board', () => {
-  it ('is instantiated', () => {
-    let board = new Board();
+  it('is instantiated', () => {
+    const board = new Board();
     expect(32).to.eql(board.size);
   });
 });
 
 describe('Board.getPieceBySq()', () => {
-  it ('a1 is a rook', () => {
-    let board = new Board();
-    let piece = board.getPieceBySq('a1');
+  it('a1 is a rook', () => {
+    const piece = new Board().getPieceBySq('a1');
     expect(piece.getId()).to.eql(Piece.R);
   });
-  it ('h8 is a rook', () => {
-    let board = new Board();
-    let piece = board.getPieceBySq('h8');
+  it('h8 is a rook', () => {
+    const piece = new Board().getPieceBySq('h8');
     expect(piece.getId()).to.eql(Piece.R);
   });
 });

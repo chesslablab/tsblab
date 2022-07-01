733a3eb8c502b1991d91f42fd10cda2fe7b469a2
programarivm <info@programarivm.com>
2022-07-01 15:21:05 +0200

    Added tests

--- tests/unit/Board.test.ts
+++ tests/unit/Board.test.ts
@@ -19,3 +19,24 @@ describe('board.getPieceBySq()', () => {
     expect(entry.value.getId()).to.eql(Piece.R);
   });
 });
+
+describe('board.play()', () => {
+  it('played short castle', () => {
+    const board = new Board();
+    board.play('w', 'e4');
+    board.play('b', 'c5');
+    board.play('w', 'Nf3');
+    board.play('b', 'Nc6');
+    expect(board.play('w', 'O-O')).to.be.false;
+  });
+  it('played long castle', () => {
+    const board = new Board();
+    board.play('w', 'e4');
+    board.play('b', 'c5');
+    board.play('w', 'Nf3');
+    board.play('b', 'Nc6');
+    board.play('w', 'Bb5');
+    board.play('b', 'Nf6');
+    expect(board.play('w', 'O-O-O')).to.be.false;
+  });
+});
